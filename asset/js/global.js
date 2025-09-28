cb6069eb44df51a42f6333a142835d40c56b93cb
its-yan <k2023_3232@sttheresia-jkt.sch.id>
2025-09-28 15:18:49 +0700

    Detailing

--- asset/js/global.js
+++ asset/js/global.js
@@ -185,35 +185,18 @@ window.kulinerData = [
         price: 45000
     },
     {
-        name: "Coto Makassar",
-        desc: "Sup daging sapi khas Makassar dengan bumbu rempah",
-        img: "asset/img/wisata andalah/pantai-mandala-2.jpeg",
-        price: 25000
+        name: "Kopi ",
+        desc: "Kopi khas Makassar yang disajikan dengan cara tradisional",
+        img: "asset3/IMG/Camping_ground/Camping Ground-3.jpeg",
+        price: 5000 - 10000
     },
     {
-        name: "Pisang Epe",
-        desc: "Pisang yang dibakar dengan gula merah dan santan",
-        img: "asset/img/wisata andalah/pantai-mandala-3.jpeg",
-        price: 15000
-    },
-    {
-        name: "Es Pisang Ijo",
-        desc: "Minuman segar dengan pisang hijau dan santan",
-        img: "asset/img/wisata andalah/pantai-mandala-4.jpeg",
-        price: 12000
-    },
-    {
-        name: "Nasi Kuning",
-        desc: "Nasi kuning dengan lauk pauk khas Sulawesi Selatan",
-        img: "asset/img/wisata andalah/pantai-mandala-5.webp",
-        price: 20000
-    },
-    {
-        name: "Sop Konro",
-        desc: "Sup iga sapi dengan bumbu khas yang menggugah selera",
-        img: "asset/img/wisata andalah/pantai-mandala-6.webp",
-        price: 30000
+        name: "Pisang kering",
+        desc: "Pisang yang dikeringkan dengan gula merah dan digoreng",
+        img: "asset3/IMG/UMKM Andalan/WhatsApp Image 2025-08-24 at 12.06.48 PM.jpeg",
+        price: 10000
     }
+
 ];
 
 // Data untuk Sewa
@@ -221,42 +204,42 @@ window.sewaData = [
     {
         name: "Motor Honda Beat",
         desc: "Motor matic yang nyaman untuk berkeliling wisata",
-        img: "asset/img/wisata andalah/pantai-mandala-7.webp",
+        img: "asset/img/wisata andalah/Motor Beat.jpeg",
         price: 75000,
         unit: "hari"
     },
     {
         name: "Mobil Avanza",
         desc: "Mobil keluarga yang nyaman untuk perjalanan wisata",
-        img: "asset/img/wisata andalah/gua-passea-1.webp",
+        img: "asset/img/wisata andalah/Toyota Avanza.jpeg",
         price: 300000,
         unit: "hari"
     },
     {
         name: "Sepeda Gunung",
         desc: "Sepeda untuk menjelajahi jalur off-road dan pantai",
-        img: "asset/img/wisata andalah/gua-passea-2.jpg",
+        img: "asset/img/wisata andalah/Motor gunung.jpeg",
         price: 50000,
         unit: "hari"
     },
     {
         name: "Perahu Nelayan",
         desc: "Perahu tradisional untuk aktivitas memancing dan wisata laut",
-        img: "asset/img/wisata andalah/gua-passea-3.jpeg",
+        img: "asset2/WhatsApp Image 2025-08-20 at 12.40.15.jpeg",
         price: 200000,
         unit: "hari"
     },
     {
         name: "Motor Scoopy",
         desc: "Motor retro yang stylish untuk berkeliling",
-        img: "asset/img/wisata andalah/tebing-mattoanging.webp",
+        img: "asset/img/wisata andalah/Motor scoopy.jpeg",
         price: 80000,
         unit: "hari"
     },
     {
         name: "Mobil Innova",
         desc: "Mobil besar yang cocok untuk rombongan wisata",
-        img: "asset/img/wisata andalah/tebing-mattoanging-2.webp",
+        img: "asset/img/wisata andalah/Mobil innova.jpeg",
         price: 400000,
         unit: "hari"
     }
